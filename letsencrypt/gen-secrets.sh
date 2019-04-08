8a61a74dfaffb18930307dbd7a496edeaf6caa14
Eamonn Hynes <eamonn@eamonnhynes.com>
2019-04-08 11:21:00 +0000

    More updates

--- letsencrypt/gen-secrets.sh
+++ letsencrypt/gen-secrets.sh
@@ -15,3 +15,10 @@ cert_name="tls-secret-"$(echo $CERTBOT_DOMAIN | sed -e 's/[_\.]/-/g')
 #echo $cert_name
 
 sudo microk8s.kubectl create secret tls $cert_name --cert /etc/letsencrypt/live/$CERTBOT_DOMAIN/fullchain.pem --key /etc/letsencrypt/live/$CERTBOT_DOMAIN/privkey.pem
+
+if [ $? -ne 0 ]; then
+    echo "Non-zero error code!"
+    echo "OK, let's attempt to update exiting Kubernetes secret..."
+    # Nice! See: https://stackoverflow.com/a/45881259/395974
+    sudo microk8s.kubectl create secret tls $cert_name --cert /etc/letsencrypt/live/$CERTBOT_DOMAIN/fullchain.pem --key /etc/letsencrypt/live/$CERTBOT_DOMAIN/privkey.pem --dry-run --save-config=false -o yaml | microk8s.kubectl apply -f -
+fi
